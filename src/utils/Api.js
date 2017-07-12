e55465cf6165caefc77bf3a94ef334c1d6771ae4
Patrick Lin <patricklin@alumni.ubc.ca>
2017-07-12 12:41:08 -0700

    refractor api code

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,27 +1,26 @@
 const PHOTOSET_ID = '72157678045313813';
 const API_KEY = '0564471d68ed3cc6268a0abbb76c7d2b';
+const API_CALL = `https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${API_KEY}&photoset_id=${PHOTOSET_ID}&extras=tags&format=json&nojsoncallback=1`;
 
-const getDescription = URL => {
-  return new Promise((resolve) => {
-    fetch(URL)
+const getDescription = URL => (
+  fetch(URL)
     .then(response => response.json())
     .then(json => json.photo.description._content)
-    .then(description => resolve(description))
-    })
-  }
+    .then(description => description)
+  );
 
-const setDescription = (photoset) => {
-  return photoset.map(img => {
-    return getDescription(img.descriptionURL)
-    .then(description => {
+const setDescription = photoset => (
+  photoset.map(img => (
+    getDescription(img.descriptionURL)
+    .then((description) => {
       img.description = description;
       return img;
     })
-  })
-}
+  ))
+);
 
 const photoParser = photoset => (
-  photoset.map(photo => {
+  photoset.map((photo) => {
     const newPhoto = Object.assign({}, photo);
     newPhoto.descriptionURL = `https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=${API_KEY}&photo_id=${photo.id}&format=json&nojsoncallback=1`;
     newPhoto.imageURL = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`;
@@ -34,18 +33,15 @@ const photoParser = photoset => (
     delete newPhoto.secret;
     return newPhoto;
   })
-)
+);
 
-const fetchImages = () => {
-  return new Promise((resolve) => {
-    const API_CALL = `https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${API_KEY}&photoset_id=${PHOTOSET_ID}&extras=tags&format=json&nojsoncallback=1`;
-    fetch(API_CALL)
+const fetchImages = () => (
+  fetch(API_CALL)
     .then(response => response.json())
     .then(json => photoParser(json.photoset.photo))
-    .then(photoset => {
-      Promise.all(setDescription(photoset)).then(completePhotoSet => resolve(completePhotoSet))
-    })
-  })
-}
+    .then(photoset => (
+      Promise.all(setDescription(photoset))
+      .then(completePhotoSet => completePhotoSet)))
+  );
 
 export default fetchImages;
