5a814a378f3194b831d09ab50f565a921cb866e0
Patrick Lin <patricklin@alumni.ubc.ca>
2017-07-17 13:48:54 -0700

    remove react-router

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router';
+import { AppContainer } from 'react-hot-loader';
 import App from './App';
 
-render((
-  <Router history={browserHistory}>
-    <Route path='/' component={App}>
-    </Route>
-  </Router>
-), document.querySelector('#app'));
+render( <AppContainer><App /></AppContainer>, document.querySelector("#app"));
+
+if (module && module.hot) {
+  module.hot.accept('./App.jsx', () => {
+    const App = require('./App.jsx').default;
+    render(
+      <AppContainer>
+        <App/>
+      </AppContainer>,
+      document.querySelector("#app")
+    );
+  });
+}
