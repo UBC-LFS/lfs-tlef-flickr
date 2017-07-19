6c22f9bc45fc29091dbf4111a5a882e1925ce9d3
Patrick Lin <patricklin@alumni.ubc.ca>
2017-07-19 15:00:17 -0700

    change value to number

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,20 +2,18 @@ import React from 'react';
 import ReactLoading from 'react-loading';
 
 const Loading = (props) => {
-  
   const loadStyle = {
     height: (props.browserHeight + 110) / 2 + 'px',
   }
-
   return (
     <ReactLoading
       className="center"
       type="spin"
       color="#444"
-      delay="0"
+      delay={0}
       style={loadStyle}
     />
   );
-} 
+};
 
 export default Loading;
