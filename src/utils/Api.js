3eae175d368463ca346baf4df9b28bc165a20705
Victor Wu <victorwu-95@hotmail.com>
2017-07-21 15:55:00 -0700

    Unfinished

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,9 +1,12 @@
 import fetch from 'isomorphic-fetch';
 import Promise from 'es6-promise-promise';
 
+// uses flickr api: photosets.getList -> photosets.getPhotos -> photos.getInfo
 const PHOTOSET_ID = '72157678045313813';
+const USER_ID = '146447925%40N07';
 const API_KEY = '0564471d68ed3cc6268a0abbb76c7d2b';
 const API_CALL = `https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${API_KEY}&photoset_id=${PHOTOSET_ID}&extras=tags&format=json&nojsoncallback=1`;
+const API_CALL_PHOTOSETS_GETLIST = `https://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=${API_KEY}&user_id=${USER_ID}&format=json&nojsoncallback=1`;
 
 const getDescription = URL => (
   fetch(URL)
@@ -11,6 +14,7 @@ const getDescription = URL => (
     .then(json => json.photo.description._content)
     .then(description => description)
 );
+
 
 const setDescription = photoset => (
   photoset.map(img => (
@@ -38,13 +42,62 @@ const photoParser = photoset => (
   })
 );
 
-const fetchImages = () => (
-  fetch(API_CALL)
+const setPhotos = albumID => {
+  const API_CALL_PHOTOSETS_GETINFO = `https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${API_KEY}&photoset_id=${albumID}&extras=tags&format=json&nojsoncallback=1`;
+  var resu = fetch(API_CALL_PHOTOSETS_GETINFO)
     .then(response => response.json())
-    .then(json => photoParser(json.photoset.photo))
-    .then(photoset => (
-      Promise.all(setDescription(photoset))
-        .then(completePhotoSet => completePhotoSet)))
+    .then(json => {
+      console.log("json: ", json.photoset.photo)
+      return json.photoset.photo})
+  return resu;
+
+};
+
+const setAlbum = photoset => {
+  var variab = photoset.map(album => {
+    console.log("albumid", album)
+    setPhotos(album.id)
+      .then(result => {console.log("res: ", result)});
+    // console.log("temp: ", temp);
+    return {albumName: album.title._content};
+      // console.log("Set: ", setPhotos(album.id))
+      // setPhotos(album.id)
+      //   .then(descrip => {
+      //     console.log("des: ", descrip)
+      //     return descrip
+      //   })
+      // var test1 = setPhotos(album.id);
+      // console.log("test1: ", test1)
+      // Promise.all(test1)
+      //   .then(result => {console.log("rere: ", result)})
+      
+      // return setPhotos(album.id).then(photos => ({albumName: album.title._content, photos}))
+        // .then(result => console.log("Result: ", result));
+      // return {albumName: album.title._content}
+      // return {albumName: album.title._content, photos: setPhotos(album.id)}
+    }
+  )
+  console.log("variabl: ", variab);
+  return variab
+};
+
+const fetchImages = () => (
+  fetch(API_CALL_PHOTOSETS_GETLIST)
+    .then(response => response.json())
+    .then(json => setAlbum(json.photosets.photoset))
+    // .then(result => console.log("Result: ", result))
+    // .then(photoset => (
+    //   Promise.all(setDescription(photoset))
+    //     .then(completePhotoSet => completePhotoSet)))
 );
+
+// const fetchImages = () => (
+//   fetch(API_CALL)
+//     .then(response => response.json())
+//     .then(json => {console.log(json); return photoParser(json.photoset.photo)})
+//     .then(photoset => { console.log("photoset: ", setDescription(photoset))
+//       return Promise.all(setDescription(photoset))
+//         .then(completePhotoSet => completePhotoSet)})
+// );
 
 export default fetchImages;
