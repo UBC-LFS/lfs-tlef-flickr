f2fad811c6beccf279e87800858f9ebd49935e1b
Patrick Lin <patricklin@alumni.ubc.ca>
2017-07-25 11:49:05 -0700

    remove search bar implementation

--- src/containers/GalleryContainer.js
+++ src/containers/GalleryContainer.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import Lightbox from 'react-images';
 import R from 'ramda';
 import fetchImages from '../utils/Api';
-import SearchBar from '../components/SearchBar';
-import SelectSearchBar from '../components/SelectSearchBar';
 import Photos from '../components/Photos';
 import Loading from '../components/Loading';
 
@@ -15,12 +13,8 @@ export default class GalleryContainer extends Component {
       imagesContainerWidth: 0,
       imageWidth: 0,
       imagesPerRow: 0,
-      selectSearch: '',
-      wordSearch: '',
       photos: [],
       visiblePhotos: [],
-      allSelectOptions: [],
-      currentSelectOptions: [],
       currentImage: 0,
       lightboxIsOpen: false,
       thumbnails: true,
@@ -30,8 +24,6 @@ export default class GalleryContainer extends Component {
     this.callAPI = this.callAPI.bind(this);
 
     this.imageController = this.imageController.bind(this);
-    this.fetchTags = this.fetchTags.bind(this);
-    this.setUniqueTags = this.setUniqueTags.bind(this);
     this.getPhotoDimensions = this.getPhotoDimensions.bind(this);
     this.addLineBreak = this.addLineBreak.bind(this);
 
@@ -44,15 +36,6 @@ export default class GalleryContainer extends Component {
     this.handleClick = this.handleClick.bind(this);
     this.openThumbnail = this.openThumbnail.bind(this);
     this.thumbnailSwitcher = this.thumbnailSwitcher.bind(this);
-
-    this.handleSelectChange = this.handleSelectChange.bind(this);
-    this.filterSelectPhotos = this.filterSelectPhotos.bind(this);
-    this.isSelectMatchingTag = this.isSelectMatchingTag.bind(this);
-    this.updateSearchTagOptions = this.updateSearchTagOptions.bind(this);
-
-    this.handleSearchChange = this.handleSearchChange.bind(this);
-    this.filterByTerm = this.filterByTerm.bind(this);
-    this.handleMultiSearch = this.handleMultiSearch.bind(this);
   }
 
   componentWillMount() {
@@ -97,8 +80,6 @@ export default class GalleryContainer extends Component {
   * @param {array} photoSet - array of photo objects
   */
   imageController(photoSet) {
-    const uniqueTags = this.fetchTags(photoSet);
-    const allSelectOptions = this.setUniqueTags(photoSet, uniqueTags);
     const photosLineBreak = this.addLineBreak(photoSet);
     const photoFinal = this.getPhotoDimensions(photosLineBreak);
     Promise.all(photoFinal)
@@ -108,54 +89,9 @@ export default class GalleryContainer extends Component {
       const sortedPhotos = sortImg(photos);
       this.setState({
         photos: sortedPhotos,
-        allSelectOptions,
-        currentSelectOptions: allSelectOptions,
         visiblePhotos: sortedPhotos,
       }, this.resizeBrowser);
     });
-  }
-
-  /**
-  * NOTE: Flickr API returns tags as an appended string
-  * Fetch all unique tags from the photo set
-  * @param {array} photoSet - array of photo objects
-  */
-  fetchTags(photoSet) {
-    const uniqueTags = new Set();
-    photoSet.forEach((photoObj) => {
-      photoObj.tags.split(' ').forEach(tag => {
-        if(tag !== '') {
-          uniqueTags.add(tag);
-        }
-      })
-    });
-    return [...(uniqueTags)].sort();
-  }
-
-  /**
-  * Creates an array of unique tag objects, sorted in descending order
-  * @param {array} photoSet - array of photo objects
-  * @param {array} uniqueTags - array of tags that are string
-  * @return {array} - array of tag objects
-  */
-  setUniqueTags(photoSet, uniqueTags) {
-    const tags = uniqueTags.map(tag => ({ value: tag, label: tag, count: 0 }));
-    const duplicateTags = [];
-    photoSet.forEach((photoObj) => {
-      photoObj.tags.split(' ').forEach(tag => (
-        duplicateTags.push(tag)
-      ));
-    });
-    tags.forEach((uniqueTag) => {
-      uniqueTag.count = duplicateTags.reduce((acc, tag) => {
-        if (tag === uniqueTag.value) {
-          acc += 1;
-        }
-        return acc;
-      }, 0);
-    });
-    const sortTags = R.sortWith([R.descend(R.prop('count'))]);
-    return sortTags(tags);
   }
 
   addDimensionsToPhotos(photosArray, photoDimensions) {
@@ -253,98 +189,6 @@ export default class GalleryContainer extends Component {
 
   /** ============ */
 
-  /**
-   * React-Select Functions
-   * ============
-  */
-  handleSelectChange(searchTerm) {
-    (searchTerm !== '')
-      ? (this.setState({ selectSearch: searchTerm }, this.filterSelectPhotos))
-      : (this.setState({ selectSearch: searchTerm }, this.filterByTerm));
-  }
-
-  filterSelectPhotos() {
-    let matchedImages = this.state.photos.filter(this.isSelectMatchingTag);
-    if (((this.state.wordSearch === '' && this.state.selectSearch === '') || this.state.selectSearch === '')) {
-      matchedImages = this.state.photos;
-    }
-    const updatedSearchOptions = this.updateSearchTagOptions();
-    const updatedCurrentSearchOptions = this.state.allSelectOptions.filter(tag => updatedSearchOptions.includes(tag.value))
-    this.setState({ visiblePhotos: matchedImages, currentSelectOptions: updatedCurrentSearchOptions }, this.handleMultiSearch);
-  }
-
-  isSelectMatchingTag(image) {
-    const tags = image.tags.split(' ');
-    const selectSearchSplit = this.state.selectSearch.split(',');
-    for (let i = 0; i < selectSearchSplit.length; i += 1) {
-      if (!tags.includes(selectSearchSplit[i])) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  updateSearchTagOptions() {
-    let otherSearchOptions = [];
-    const searchTags = ((this.state.selectSearch === '') ? [] : this.state.selectSearch.split(','));
-    if (searchTags.length === 0) {
-      otherSearchOptions = this.fetchTags(this.state.photos);
-    } else {
-      this.state.photos.map((photo) => {
-        let tagTest = true;
-        const photoTags = photo.tags.split(' ');
-        if (photoTags.length > searchTags.length) {
-          for (let i = 0; i < searchTags.length; i += 1) {
-            if (!photoTags.includes(searchTags[i])) {
-              tagTest = false;
-              break;
-            }
-          }
-          if (tagTest === true) {
-            for (let i = 0; i < photoTags.length; i += 1) {
-              if (!searchTags.includes(photoTags[i]) && !otherSearchOptions.includes(photoTags[i])) {
-                otherSearchOptions.push(photoTags[i]);
-              }
-            }
-          }
-        }
-        return photo;
-      });
-      otherSearchOptions.push(...searchTags);
-    }
-    return otherSearchOptions;
-  }
-
-  /** ============ */
-
-  /**
-   * React-Search-Input Functions
-   * ============
-  */
-
-  filterByTerm() {
-    const photoSet = (this.state.selectSearch === '' ? this.state.photos : this.state.visiblePhotos);
-    const searchKey = this.state.wordSearch.toUpperCase();
-    const matchedImages = photoSet.filter(photo => ((photo.title.toUpperCase().includes(searchKey) || photo.description.toUpperCase().includes(searchKey))));
-    const updatedSearchOptions = this.updateSearchTagOptions();
-    const updatedCurrentSearchOptions = this.state.allSelectOptions.filter(tag => updatedSearchOptions.includes(tag.value))
-    this.setState({ visiblePhotos: matchedImages, currentSelectOptions: updatedCurrentSearchOptions });
-  }
-
-  handleMultiSearch() {
-    if ((this.state.selectSearch !== '' && this.state.wordSearch !== '')) {
-      this.filterByTerm();
-    }
-  }
-
-  handleSearchChange(searchTerm) {
-    (this.state.selectSearch !== '')
-      ? (this.setState({ wordSearch: searchTerm }, this.filterSelectPhotos))
-      : (this.setState({ wordSearch: searchTerm }, this.filterByTerm));
-  }
-
-  /** ============ */
-
   render() {
     const lightboxPhotos = this.getLightboxImages(this.state.visiblePhotos);
     const thumbnails = this.thumbnailSwitcher();
@@ -353,21 +197,7 @@ export default class GalleryContainer extends Component {
         <div className="navbar">
           <div className="navbar-inner">
             <div className="container">
-              <div className="row-fluid">
-                <div className="span6">
-                  <SearchBar
-                    className="search-input"
-                    onSearchChange={this.handleSearchChange}
-                  />
-                </div>
-                <div className="span6">
-                  <SelectSearchBar
-                    currentSearch={this.state.selectSearch}
-                    onSelectChange={this.handleSelectChange}
-                    selectOptions={this.state.currentSelectOptions}
-                  />
-                </div>
-              </div>
+              <h1>UBC LFS</h1>
             </div>
           </div>
         </div>
