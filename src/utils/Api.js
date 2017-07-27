03ce21e5f9ddedc4e6ef976a334ea000bab54088
Victor Wu <victorwu-95@hotmail.com>
2017-07-27 12:01:59 -0700

    Completed API function to add photos under album

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -2,45 +2,10 @@ import fetch from 'isomorphic-fetch';
 import Promise from 'es6-promise-promise';
 
 // uses flickr api: photosets.getList -> photosets.getPhotos -> photos.getInfo
-const PHOTOSET_ID = '72157678045313813';
 const USER_ID = '146447925%40N07';
 const API_KEY = '0564471d68ed3cc6268a0abbb76c7d2b';
-const API_CALL = `https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${API_KEY}&photoset_id=${PHOTOSET_ID}&extras=tags&format=json&nojsoncallback=1`;
 const API_CALL_PHOTOSETS_GETLIST = `https://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=${API_KEY}&user_id=${USER_ID}&format=json&nojsoncallback=1`;
 
-const getDescription = URL => (
-  fetch(URL)
-    .then(response => response.json())
-    .then(json => json.photo.description._content)
-    .then(description => description)
-);
-
-
-const setDescription = photoset => (
-  photoset.map(img => (
-    getDescription(img.descriptionURL)
-      .then((description) => {
-        img.description = description;
-        return img;
-      })
-  ))
-);
-
-const photoParser = photoset => (
-  photoset.map((photo) => {
-    const newPhoto = Object.assign({}, photo);
-    newPhoto.descriptionURL = `https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=${API_KEY}&photo_id=${photo.id}&format=json&nojsoncallback=1`;
-    newPhoto.imageURL = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`;
-    delete newPhoto.isfamily;
-    delete newPhoto.isfriend;
-    delete newPhoto.isprimary;
-    delete newPhoto.server;
-    delete newPhoto.farm;
-    delete newPhoto.ispublic;
-    delete newPhoto.secret;
-    return newPhoto;
-  })
-);
 
 const getPhotos = albumID => {
   const API_CALL_PHOTOSETS_GETPHOTO = `https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${API_KEY}&photoset_id=${albumID}&extras=tags&format=json&nojsoncallback=1`;
@@ -49,15 +14,14 @@ const getPhotos = albumID => {
     .then(json => {return json.photoset.photo})
 };
 
-const setAlbum = photoset => (
-  photoset.map(album => {
+const setAlbum = albumInfo => {
+  return albumInfo.map(album => {
     return getPhotos(album.id)
-      .then(result => {
-        console.log("res", result)
-        return {albumName: album.title._content, photos: result}
+      .then(photos => {
+        return {albumName: album.title._content, photos}
       });
     })
-);
+};
 
 const getPhotosDescription = photoInfoURL => (
   fetch(photoInfoURL)
@@ -65,11 +29,9 @@ const getPhotosDescription = photoInfoURL => (
     .then(json => json.photo.description._content)
 )
 
-const setPhotosDescription = albumSet => {
-  console.log("albumSet: ", albumSet);
+const setPhotosDescription = albumSet => (
   albumSet.photos.map(photo => {
     const descriptionURL = `https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=${API_KEY}&photo_id=${photo.id}&format=json&nojsoncallback=1`;
-    // console.log("photo: ", photo)
     const tempPhoto = Object.assign({}, photo);
     tempPhoto.imageURL = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`;
     delete tempPhoto.isfamily;
@@ -85,45 +47,26 @@ const setPhotosDescription = albumSet => {
         return tempPhoto;
       })
   })
-}
+)
 
 const fetchImages = () => (
   fetch(API_CALL_PHOTOSETS_GETLIST)
     .then(response => response.json())
     .then(json => setAlbum(json.photosets.photoset))
-    .then(result => {
-      // return Promise.all(result)
-      // console.log("Result: ", result)
-      Promise.all(result)
-        .then(albumNoDescription => {
-          console.log("Album: ", albumNoDescription);
-          return albumNoDescription.map(albumSet => {
-            return setPhotosDescription(albumSet);
-          });
-        })
-        // .then(test => console.log("tes: ", test))
-
-    })
-    // .then(albumNoDescription => {
-    //   return albumNoDescription.map(albumSet => {
-    //     return setPhotosDescription(albumSet);
-    //   })
-    // })
-    // .then(albumFinal => {
-    //   console.log("here: ", albumFinal)
-    // })
-    // .then(photoset => (
-    //   Promise.all(setDescription(photoset))
-    //     .then(completePhotoSet => completePhotoSet)))
+    .then(photoSet => (
+      Promise.all(photoSet)
+        .then(photoSetNoDescription => photoSetNoDescription)
+    ))
+    .then(albumsPhotosNoDescription => (
+      albumsPhotosNoDescription.map(albumPhotos => {
+        return Promise.all(setPhotosDescription(albumPhotos))
+          .then(albumOfPhotosPromise => albumOfPhotosPromise)
+      })
+    ))
+    .then(albumsPhotosWithDescription => (
+      Promise.all(albumsPhotosWithDescription)
+        .then(albumFinal => albumFinal)
+    ))
 );
-
-// const fetchImages = () => (
-//   fetch(API_CALL)
-//     .then(response => response.json())
-//     .then(json => {console.log(json); return photoParser(json.photoset.photo)})
-//     .then(photoset => { console.log("photoset: ", setDescription(photoset))
-//       return Promise.all(setDescription(photoset))
-//         .then(completePhotoSet => completePhotoSet)})
-// );
 
 export default fetchImages;
