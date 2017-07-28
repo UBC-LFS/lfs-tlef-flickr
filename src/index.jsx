fcbc4f38e13c9a9bc13633d149b448d07a7f4405
Patrick Lin <patricklin@alumni.ubc.ca>
2017-07-28 12:15:25 -0700

    webpack 3 implementation

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,10 @@ import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import App from './App';
 
-render( <AppContainer><App /></AppContainer>, document.querySelector("#app"));
+render( <AppContainer><App/></AppContainer>, document.querySelector("#app"));
 
 if (module && module.hot) {
-  module.hot.accept('./App.jsx', () => {
-    const App = require('./App.jsx').default;
+  module.hot.accept('./App', () => {
     render(
       <AppContainer>
         <App/>
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -5,8 +5,8 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var WebpackCleanupPlugin = require('webpack-cleanup-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-loaders.push({ 
-	test: /\.scss$/, 
+loaders.push({
+	test: /\.scss$/,
 	loader: ExtractTextPlugin.extract('style', 'css?sourceMap&localIdentName=[local]___[hash:base64:5]!sass?outputStyle=expanded'),
 	exclude: ['node_modules']
 });
@@ -14,15 +14,15 @@ loaders.push({
 module.exports = {
 	entry: [
 		'./src/index.jsx',
-		'./styles/index.scss'
+		'./styles/index.scss',
 	],
 	output: {
-		publicPath: '/',
+		publicPath: './',
 		path: path.join(__dirname, 'public'),
 		filename: '[chunkhash].js'
 	},
 	resolve: {
-		extensions: ['', '.js', '.jsx']
+		extensions: ['.js', '.jsx']
 	},
 	module: {
 		loaders
@@ -43,10 +43,10 @@ module.exports = {
 			}
 		}),
 		new webpack.optimize.OccurenceOrderPlugin(),
-		new webpack.optimize.DedupePlugin(),
-	    new ExtractTextPlugin("style.css", {
-		      allChunks: true
-		}),
+		new ExtractTextPlugin({
+      filename: 'style.css',
+      allChunks: true
+    }),
 		new HtmlWebpackPlugin({
 			template: './src/template.html',
 			files: {
