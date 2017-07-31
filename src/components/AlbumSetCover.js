9f21e029e4088d201dcc9ebe0c1aa7b55c21af48
Victor Wu <victorwu-95@hotmail.com>
2017-07-31 12:21:45 -0700

    Added Album Title effects

--- src/components/AlbumSetCover.js
+++ src/components/AlbumSetCover.js
@@ -2,31 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const AlbumSetCover = (props) => {
-    // const imageCoverCounter = () => {
-    //     let count = 0;
-    //     switch(true)
-    //     {
-    //         case (props.albumInfo.albumDetails.photo.length === 1):
-    //             count = 1;
-    //             break;
-    //         case (props.albumInfo.albumDetails.photo.length === 2):
-    //             count = 2;
-    //             break;
-    //         case (props.albumInfo.albumDetails.photo.length >= 3):
-    //             count = 3;
-    //             break;
-    //         default:
-    //             count = 0;
-    //     }
-    //     return (
-    //         count
-    //     )
-    // }
-
-    const imageURLLink = () => {
-        console.log(props);
-        return props.albumInfo.albumDetails.photo[0].imageURL
-    }
 
     const albumCoverSize = () => {
         return {
@@ -51,12 +26,15 @@ const AlbumSetCover = (props) => {
             <div className="albumCoverName">
                 <span>{props.albumInfo.albumName}</span>
             </div>
+            <div className="albumCoverDescription">
+                <span>Photos</span>
+            </div>
             <div className="albumOuterContainer">
                 <div className="albumInnerContainer">
                     <img
                         className={"coverImage" + (props.albumInfo.albumDetails.photo[0].orientation ===
                         "landscape" ? " fullHeight" : " fullWIdth")}
-                        src={imageURLLink()}
+                        src={props.albumInfo.albumDetails.photo[0].imageURL}
                     />
                 </div>
             </div>
