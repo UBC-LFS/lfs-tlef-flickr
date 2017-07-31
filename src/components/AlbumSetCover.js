fbf8f767c6386880ee669e3343fe0cefd462939d
Victor Wu <victorwu-95@hotmail.com>
2017-07-31 09:57:12 -0700

    Display one picture for album

--- src/components/AlbumSetCover.js
+++ src/components/AlbumSetCover.js
@@ -2,31 +2,40 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const AlbumSetCover = (props) => {
-    const imageCoverCounter = () => {
-        let count = 0;
-        switch(true)
-        {
-            case (props.albumInfo.albumDetails.photo.length === 1):
-                count = 1;
-                break;
-            case (props.albumInfo.albumDetails.photo.length === 2):
-                count = 2;
-                break;
-            case (props.albumInfo.albumDetails.photo.length >= 3):
-                count = 3;
-                break;
-            default:
-                count = 0;
-        }
-        return (
-            count
-        )
-    }
+    // const imageCoverCounter = () => {
+    //     let count = 0;
+    //     switch(true)
+    //     {
+    //         case (props.albumInfo.albumDetails.photo.length === 1):
+    //             count = 1;
+    //             break;
+    //         case (props.albumInfo.albumDetails.photo.length === 2):
+    //             count = 2;
+    //             break;
+    //         case (props.albumInfo.albumDetails.photo.length >= 3):
+    //             count = 3;
+    //             break;
+    //         default:
+    //             count = 0;
+    //     }
+    //     return (
+    //         count
+    //     )
+    // }
 
     const albumCoverSize = {
         width: props.albumSize + 'px',
         height: props.albumSize + 'px'
     }
+            /*{Array.apply(null, Array(imageCoverCounter())).map((item, index) => {
+                return (
+                    <img
+                        key={index} 
+                        className="coverImage"
+                        src={props.albumInfo.albumDetails.photo[index].imageURL}
+                    />
+                )
+            })}*/
 
     return (
         <div
@@ -35,15 +44,10 @@ const AlbumSetCover = (props) => {
             <div>
                 <span>{props.albumInfo.albumName}</span>
             </div>
-            {Array.apply(null, Array(imageCoverCounter())).map((item, index) => {
-                return (
-                    <img
-                        key={index} 
-                        className="coverImage"
-                        src={props.albumInfo.albumDetails.photo[index].imageURL}
-                    />
-                )
-            })}
+            <img
+                className="coverImage"
+                src={props.albumInfo.albumDetails.photo[0].imageURL}
+            />
         </div>
     )
 }
