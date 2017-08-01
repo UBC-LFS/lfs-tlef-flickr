cd53129632e8672eaa95843eb7f6771ea9ddfafd
Patrick Lin <patricklin@alumni.ubc.ca>
2017-08-01 16:03:40 -0700

    404 error page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import AlbumContainer from './containers/AlbumContainer';
 import GalleryContainer from './containers/GalleryContainer';
 
 const App = () => (
   <Router>
     <div>
+      <Switch>
       <Route exact path="/" component={AlbumContainer} />
       <Route path="/album" component={GalleryContainer} />
+      <Route render={() => <h1 className="fourZeroFour">404: Page Not Found </h1>} />
+       </Switch>
     </div>
   </Router>
 );
