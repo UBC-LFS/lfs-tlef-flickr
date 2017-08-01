3219737d4adfa319d06b252f0383e8da7cb9b619
Patrick Lin <patricklin@alumni.ubc.ca>
2017-08-01 11:47:09 -0700

    clean up code

--- src/components/AlbumSetCover.js
+++ src/components/AlbumSetCover.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 const AlbumSetCover = (props) => {
 
-    const albumCoverSize = () => {
-        return {
-            width: props.albumSize + 'px',
-            height: props.albumSize + 'px'
-        }
-    }
-            /*{Array.apply(null, Array(imageCoverCounter())).map((item, index) => {
+	const albumCoverSize = () => {
+		return {
+			width: props.albumSize + 'px',
+			height: props.albumSize + 'px'
+		}
+	}
+	/*{Array.apply(null, Array(imageCoverCounter())).map((item, index) => {
                 return (
                     <img
                         key={index}
@@ -20,40 +20,35 @@ const AlbumSetCover = (props) => {
                 )
             })}*/
 
-    return (
-        <div
-            style={albumCoverSize()}
-            className="albumCover">
+	return (
+		<div style={albumCoverSize()} className="albumCover">
 
-            <div className="albumCoverName">
-                <span>{props.albumInfo.albumName}</span>
-            </div>
-            <div className="albumCoverDescription">
-                <span>Photos</span>
-            </div>
-            <div className="albumOuterContainer">
-              <Link
-                  to={{
-                    pathname: `/album`,
-                    search: `?albumName=${props.albumInfo.albumName}`
-                  }}>
+			<div className="albumCoverName">
+				<span>{props.albumInfo.albumName}</span>
+			</div>
+			<div className="albumCoverDescription">
+				<span>Photos</span>
+			</div>
+			<div className="albumOuterContainer">
+				<Link to={{
+					pathname: `/album`,
+					search: `?albumName=${props.albumInfo.albumName}`
+				}}>
 
-                <div className="albumInnerContainer">
-                  <img
-                        className={"coverImage" + (props.albumInfo.albumDetails.photo[0].orientation ===
-                        "landscape" ? " fullHeight" : " fullWIdth")}
-                        src={props.albumInfo.albumDetails.photo[0].imageURL}
-                    />
-                </div>
-                </Link>
-            </div>
+					<div className="albumInnerContainer">
+						<img className={"coverImage" + (props.albumInfo.albumDetails.photo[0].orientation === "landscape"
+							? " fullHeight"
+							: " fullWIdth")} src={props.albumInfo.albumDetails.photo[0].imageURL}/>
+					</div>
+				</Link>
+			</div>
 
-        </div>
-    )
+		</div>
+	)
 }
 
 AlbumSetCover.PropTypes = {
-    albumInfo: PropTypes.array.isRequired
+	albumInfo: PropTypes.array.isRequired
 }
 
 export default AlbumSetCover;
