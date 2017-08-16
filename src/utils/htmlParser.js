e2fd096171035ee8f08be3c8e00cf3b836924835
patricklinpl <patricklin@alumni.ubc.ca>
2017-08-16 09:04:58 -0700

    template API key

--- src/utils/htmlParser.js
+++ src/utils/htmlParser.js
@@ -1,15 +1,8 @@
 import fetch from 'isomorphic-fetch';
-import Promise from 'es6-promise-promise';
-import R from 'ramda';
 
-const API_CALL = "https://sheets.googleapis.com/v4/spreadsheets/1wmZ7HuTM941dqUFmnsViP3lVkRpxjyjz0U0RasR1rR4?includeGridData=true&key=AIzaSyAb5iemYiKoj96gVztUPy_M1yNQmkfQBL8"
-
-const fetchDefinitions = () => (
-  fetch(API_CALL)
-    .then(response => response.json())
-    .then(json => json.sheets[0].data[0].rowData)
-    .then(definitions => parser(definitions))
-);
+const SHEET_ID = "1wmZ7HuTM941dqUFmnsViP3lVkRpxjyjz0U0RasR1rR4";
+const API_KEY = "AIzaSyBw3Tmv6G69vJF0USujX77ZUjQvFNz7OPw";
+const API_CALL = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}?includeGridData=true&key=${API_KEY}`;
 
 const parser = definitions => {
   definitions.splice(0, 1);
@@ -23,9 +16,7 @@ const parser = definitions => {
     if (typeof keyTerm !== 'undefined') {
       if (typeof defobj.values[1].textFormatRuns !== 'undefined') {
         defobj.values[1].textFormatRuns.splice(0, 1);
-
         let formattingArray = [];
-
         for (let i = 0; i < defobj.values[1].textFormatRuns.length; i += 2) {
           let format = Object.keys(defobj.values[1].textFormatRuns[i].format);
           const first = defobj.values[1].textFormatRuns[i].startIndex;
@@ -35,21 +26,26 @@ const parser = definitions => {
           formatObj[format] = replaceWord;
           formattingArray.push(formatObj);
         }
-
         formattingArray.forEach(formatObj => {
-          if (Object.keys(formatObj)[0] == "bold") {
+          if (Object.keys(formatObj)[0] === "bold") {
             keyDef = keyDef.replace(formatObj.bold, '<b>' + formatObj.bold + '</b>');
           }
-          if (Object.keys(formatObj)[0] == "italic") {
+          if (Object.keys(formatObj)[0] === "italic") {
             keyDef = keyDef.replace(formatObj.italic, '<i>' + formatObj.italic + '</i>');
           }
         });
       }
       dictionaryObj[keyTerm] = keyDef;
-    } 
+    }
   });
-  
   return dictionaryObj;
 }
+
+const fetchDefinitions = () => (
+  fetch(API_CALL)
+    .then(response => response.json())
+    .then(json => json.sheets[0].data[0].rowData)
+    .then(definitions => parser(definitions))
+);
 
 export default fetchDefinitions;
